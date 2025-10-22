7b53ee708f7b8331336ed13db956425ccc5e21e8
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-10-22 07:51:51 +0530

    HomePage Home Section-1 code added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,118 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import HighlightText from "../components/core/HomePage/HighlightText";
+import CTAButton from "../components/core/HomePage/Button";
+import Banner from "../assets/Images/banner.mp4";
+import CodeBlocks from "../components/core/HomePage/CodeBlocks";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      {/* Section 1 */}
+      <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-between max-w-maxContent">
+        <Link to={"/signup"}>
+          <div className="mt-16 group p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit">
+            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
+              <p>Become an Instructor</p>
+              <FaArrowRight />
+            </div>
+          </div>
+        </Link>
 
-export default Home
+        <div className="text-center text-3xl lg:text-4xl font-semibold mt-7">
+          Empower Your Future with
+          <HighlightText text={"Coding Skills"} />
+        </div>
+
+        <div className="mt-4 w-[90%] text-center text-base lg:text-lg font-bold text-richblack-300">
+          With our online coding courses, you can learn at your own pace, from
+          anywhere in the world, and get access to a wealth of resources,
+          including hands-on projects, quizzes, and personalized feedback from
+          instructors.
+        </div>
+
+        <div className="flex gap-7 mt-8">
+          <CTAButton active={true} linkto={"/signup"}>
+            Learn More
+          </CTAButton>
+
+          <CTAButton active={false} linkto={"/login"}>
+            Book a Demo
+          </CTAButton>
+        </div>
+
+        <div className="shadow-blue-200 mx-3 my-12">
+          <video muted loop autoPlay>
+            <source src={Banner} type="video/mp4" />
+          </video>
+        </div>
+
+        {/* Code Section 1 */}
+        <div>
+          <CodeBlocks
+            position={"lg:flex"}
+            heading={
+              <div className="text-4xl font-semibold">
+                Unlock your
+                <HighlightText text={"coding potential"} /> with our online
+                courses.
+              </div>
+            }
+            subheading={
+              "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+            }
+            ctabtn1={{
+              btnText: "Try it Yourself",
+              linkto: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "Learn More",
+              linkto: "/login",
+              active: false,
+            }}
+            codeblock={`<!DOCTYPE html>\n<html>\n<head><title>Example</title>\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\n<nav><ahref="one/">One</a><ahref="two/">Two</a><ahref="three/">Three</a>\n</nav>`}
+            codeColor={`text-yellow-25`}
+          />
+        </div>
+
+        {/* Code Section 2 */}
+        <div>
+          <CodeBlocks
+            position={"lg:flex-row-reverse"}
+            heading={
+              <div className="text-4xl font-semibold">
+                Start
+                <HighlightText text={"coding in seconds"} />
+              </div>
+            }
+            subheading={
+              "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+            }
+            ctabtn1={{
+              btnText: "Continue Lesson",
+              linkto: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "learn more",
+              linkto: "/login",
+              active: false,
+            }}
+            codeblock={`<!DOCTYPE html>\n<html>\n<head><title>Example</title>\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\n<nav><ahref="one/">One</a><ahref="two/">Two</a><ahref="three/">Three</a>\n</nav>`}
+            codeColor={"text-yellow-25"}
+          />
+        </div>
+      </div>
+
+      {/* Section 2 */}
+
+      {/* Section 3 */}
+
+      {/* Footer Section */}
+    </div>
+  );
+};
+
+export default Home;
