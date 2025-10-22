90437d80938293058964e6cd1a2e43a3b55d42bc
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-10-22 15:05:59 +0530

    HomePage Home Section-2 code added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { FaArrowRight } from "react-icons/fa";
-import HighlightText from "../components/core/HomePage/HighlightText";
 import CTAButton from "../components/core/HomePage/Button";
+import { FaArrowRight } from "react-icons/fa";
 import Banner from "../assets/Images/banner.mp4";
+import HighlightText from "../components/core/HomePage/HighlightText";
 import CodeBlocks from "../components/core/HomePage/CodeBlocks";
+import TimelineSection from "../components/core/HomePage/TimeLineSection";
+import LearningLanguageSection from "../components/core/HomePage/LearningLanguageSection";
 
-const Home = () => {
+function Home() {
   return (
     <div>
       {/* Section 1 */}
-      <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-between max-w-maxContent">
-        <Link to={"/signup"}>
-          <div className="mt-16 group p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit">
-            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
-              <p>Become an Instructor</p>
-              <FaArrowRight />
-            </div>
-          </div>
-        </Link>
-
-        <div className="text-center text-3xl lg:text-4xl font-semibold mt-7">
-          Empower Your Future with
-          <HighlightText text={"Coding Skills"} />
+      <div className=" mx-auto relative flex flex-col w-11/12 items-center justify-between text-white ">
+        <div className="text-center text-3xl md:text-4xl font-semibold mt-7">
+          Empower Your Future With <HighlightText text={"Coding Skills"} />
         </div>
-
-        <div className="mt-4 w-[90%] text-center text-base lg:text-lg font-bold text-richblack-300">
+        <div className=" mt-4 w-[90%] text-left md:text-center text-sm md:text-lg font-bold text-richblack-300">
           With our online coding courses, you can learn at your own pace, from
           anywhere in the world, and get access to a wealth of resources,
           including hands-on projects, quizzes, and personalized feedback from
           instructors.
         </div>
 
-        <div className="flex gap-7 mt-8">
+        <div className="flex flex-row gap-7 mt-8">
           <CTAButton active={true} linkto={"/signup"}>
             Learn More
           </CTAButton>
@@ -42,8 +32,9 @@ const Home = () => {
           </CTAButton>
         </div>
 
-        <div className="shadow-blue-200 mx-3 my-12">
-          <video muted loop autoPlay>
+        <div className="mx-3 my-12 shadow-blue-200 w-[70%] relative">
+          <div className="grad2 -top-10 w-[800px]"></div>
+          <video className="video" muted loop autoPlay>
             <source src={Banner} type="video/mp4" />
           </video>
         </div>
@@ -51,19 +42,19 @@ const Home = () => {
         {/* Code Section 1 */}
         <div>
           <CodeBlocks
-            position={"lg:flex"}
+            position={"lg:flex-row"}
             heading={
-              <div className="text-4xl font-semibold">
-                Unlock your
-                <HighlightText text={"coding potential"} /> with our online
-                courses.
+              <div className=" font-semibold text-2xl lg:text-4xl sm:w-full">
+                Unlock Your
+                <HighlightText text={"coding potential"} />
+                with our online courses
               </div>
             }
             subheading={
               "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
             }
             ctabtn1={{
-              btnText: "Try it Yourself",
+              btnText: "Try it yourself",
               linkto: "/signup",
               active: true,
             }}
@@ -73,7 +64,7 @@ const Home = () => {
               active: false,
             }}
             codeblock={`<!DOCTYPE html>\n<html>\n<head><title>Example</title>\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\n<nav><ahref="one/">One</a><ahref="two/">Two</a><ahref="three/">Three</a>\n</nav>`}
-            codeColor={`text-yellow-25`}
+            codeColor={"text-yellow-25"}
           />
         </div>
 
@@ -107,12 +98,50 @@ const Home = () => {
       </div>
 
       {/* Section 2 */}
+      <div className="bg-pure-greys-5 text-richblack-700">
+        <div className="homepage_bg h-[310px]">
+          <div className="w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto">
+            <div className="h-[150px]"></div>
+            <div className="flex flex-row gap-7 text-white ">
+              <CTAButton active={true} linkto={"/signup"}>
+                <div className="flex items-center gap-3">
+                  Explore Full Catalog
+                  <FaArrowRight />
+                </div>
+              </CTAButton>
+              <CTAButton active={false} linkto={"/signup"}>
+                <div>Learn more</div>
+              </CTAButton>
+            </div>
+          </div>
+        </div>
 
-      {/* Section 3 */}
+        <div className="mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7">
+          <div className="flex flex-row gap-5 mb-10 mt-[95px]">
+            <div className="text-4xl font-semibold w-[45%]">
+              Get the Skills you need for a
+              <HighlightText text={"Job that is in demand"} />
+            </div>
 
-      {/* Footer Section */}
+            <div className="flex flex-col gap-10 w-[40%] items-start">
+              <div className="text-[16px]">
+                The modern StudyNotion is the dictates its own terms. Today, to
+                be a competitive specialist requires more than professional
+                skills.
+              </div>
+              <CTAButton active={true} linkto={"/signup"}>
+                <div>Learn more</div>
+              </CTAButton>
+            </div>
+          </div>
+
+          <TimelineSection />
+
+          <LearningLanguageSection />
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default Home;
