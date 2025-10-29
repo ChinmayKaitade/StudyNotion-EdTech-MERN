94a3a23675e54de19b2ff6ebb07972cd425d4053
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-10-29 07:17:37 +0530

    Login and Signup page code added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/common/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import OpenRoute from "./components/core/Auth/OpenRoute"
 
 function App() {
   return (
@@ -8,6 +11,24 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+
+        <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
       </Routes>
     </div>
   );
--- src/slices/profileSlice.js
+++ src/slices/profileSlice.js
@@ -4,10 +4,11 @@ const initialState = {
   user: localStorage.getItem("user")
     ? JSON.parse(localStorage.getItem("user"))
     : null,
+  loading: false,
 };
 
-// Problem occurred and solved
-// initially i mark user data as null
+// Problem occured and solved
+// initially i mark user data as nulll
 // as i refresh the page user becomes null , so login / signup buttons are not visible - In case of user not logged
 // case - User logged but as i refresh , dashboard dropdown becomes invisible
 // solution - try getting value from localStorage otherwise mark it as null
@@ -19,8 +20,11 @@ const profileSlice = createSlice({
     setUser(state, value) {
       state.user = value.payload;
     },
+    setLoading(state, value) {
+      state.loading = value.payload;
+    },
   },
 });
 
-export const { setUser } = profileSlice.actions;
+export const { setUser, setLoading } = profileSlice.actions;
 export default profileSlice.reducer;
